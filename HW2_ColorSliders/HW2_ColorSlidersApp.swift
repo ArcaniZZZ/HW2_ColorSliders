90fb1aa3202e38470e36d8361a1a073cf98df277
ArcaniZZZ <arcani91@gmail.com>
2022-09-11 23:13:00 +0300

    Initial Commit

---
+++ HW2_ColorSliders/HW2_ColorSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW2_ColorSlidersApp.swift
+//  HW2_ColorSliders
+//
+//  Created by Alexander Zubach on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW2_ColorSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
